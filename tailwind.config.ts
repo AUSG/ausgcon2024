7d625c23127442088916ba13a4351bba3919f093
Grayash <k_2684@naver.com>
2024-08-18 11:35:44 +0900

    Add new colors and container settings to tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        xl: "1280px",
+      },
+    },
     extend: {
-      colors: {},
+      colors: {
+        primary: "#F4862C",
+      },
+      backgroundColor: {
+        scene: "#CDF6FF",
+      },
     },
   },
   plugins: [],
