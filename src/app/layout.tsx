72586e2ae70ddf3973c96e8a958d5b57c9c2966f
Grayash <k_2684@naver.com>
2024-08-18 10:57:40 +0900

    Init `SUIT` font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const suit = localFont({
+  src: "./fonts/SUIT-Variable.woff2",
+  display: "swap",
+  weight: "45 920",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={suit.className}>{children}</body>
     </html>
   );
 }
