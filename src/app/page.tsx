d7fc820ec9511cbc046e63ea9670c968af98a44c
Grayash <k_2684@naver.com>
2024-08-18 18:24:03 +0900

    Add SessionSection component to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Background from "./components/Background";
 import HeroSection from "./components/HeroSection";
+import SessionSection from "./components/SessionSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <SessionSection />
       <Background />
     </main>
   );
