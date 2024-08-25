4df3e945eec448ed39bdf8bbf61a5bbda1f1fed9
Grayash <k_2684@naver.com>
2024-08-25 16:28:27 +0900

    Refactor SectionList for ausgcon

--- src/app/components/SessionSection/SectionList.tsx
+++ src/app/components/SessionSection/SectionList.tsx
@@ -1,107 +1,79 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-import FadeIn from "../FadeIn";
+"use client";
 
-const sessions = [
-  {
-    name: "A홀",
-    posts: [
-      {
-        id: 1,
-        title: "Does drinking coffee make you smarter?",
-        date: "5h ago",
-        commentCount: 5,
-        shareCount: 2,
-      },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: "2h ago",
-        commentCount: 3,
-        shareCount: 2,
-      },
-    ],
-  },
-  {
-    name: "B홀",
-    posts: [
-      {
-        id: 1,
-        title: "Is tech making coffee better or worse?",
-        date: "Jan 7",
-        commentCount: 29,
-        shareCount: 16,
-      },
-      {
-        id: 2,
-        title: "The most innovative things happening in coffee",
-        date: "Mar 19",
-        commentCount: 24,
-        shareCount: 12,
-      },
-    ],
-  },
-  {
-    name: "부스",
-    posts: [
-      {
-        id: 1,
-        title: "Ask Me Anything: 10 answers to your questions about coffee",
-        date: "2d ago",
-        commentCount: 9,
-        shareCount: 5,
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-  },
-];
+import { useState } from "react";
+import Image from "next/image";
+import { sessionList, sessionTimeLabelLookup } from "@/app/constants/session";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { ClockIcon } from "@heroicons/react/24/outline";
+import FadeIn from "../FadeIn";
+import SessionDialog from "./SessionDialog";
 
 export default function SectionList() {
+  const [isSessionDialogOpen, setIsSessionDialogOpen] = useState(false);
+  const [selectedSession, setSelectedSession] =
+    useState<(typeof sessionList)[number]["sessions"][number]>();
+
   return (
     <FadeIn className="flex h-screen w-full justify-center px-4 pt-10">
       <div className="w-full max-w-md">
         <TabGroup>
-          <TabList className="flex gap-4">
-            {sessions.map(({ name }) => (
+          <TabList className="flex justify-center gap-4">
+            {sessionList.map(({ track }) => (
               <Tab
-                key={name}
+                key={track}
                 className="rounded-full px-3 py-1 text-sm/6 font-semibold text-white focus:outline-none data-[hover]:bg-white/5 data-[selected]:bg-white/10 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
               >
-                {name}
+                {track}
               </Tab>
             ))}
           </TabList>
           <TabPanels className="mt-3">
-            {sessions.map(({ name, posts }) => (
-              <TabPanel key={name} className="rounded-xl bg-white/20 p-3">
-                <ul>
-                  {posts.map((post) => (
-                    <li
-                      key={post.id}
-                      className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5"
-                    >
-                      <a href="#" className="font-semibold text-white">
-                        <span className="absolute inset-0" />
-                        {post.title}
-                      </a>
-                      <ul
-                        className="flex gap-2 text-white/50"
-                        aria-hidden="true"
+            {sessionList.map(({ track, sessions }) => (
+              <TabPanel
+                key={track}
+                className="rounded-xl bg-white/20 p-3 focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
+              >
+                <div>
+                  {sessions.map((session) => (
+                    <div key={session.order}>
+                      <span className="flex items-center gap-1 text-xl text-white">
+                        <ClockIcon className="size-4" />
+                        {sessionTimeLabelLookup[session.order]}
+                      </span>
+                      <div
+                        className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5"
+                        onClick={() => {
+                          setSelectedSession(session);
+                          setIsSessionDialogOpen(true);
+                        }}
                       >
-                        <li>{post.date}</li>
-                        <li aria-hidden="true">&middot;</li>
-                        <li>{post.commentCount} comments</li>
-                        <li aria-hidden="true">&middot;</li>
-                        <li>{post.shareCount} shares</li>
-                      </ul>
-                    </li>
+                        <span className="font-semibold text-white">
+                          {session.title}
+                        </span>
+                        <ul
+                          className="flex gap-2 text-white/50"
+                          aria-hidden="true"
+                        >
+                          <Image
+                            className="rounded-full"
+                            src={session.speakerImage}
+                            alt={session.speaker}
+                            width={24}
+                            height={24}
+                          />
+                          <span>{session.speaker}</span>
+                          <span aria-hidden="true">&middot;</span>
+                          <span>{session.speakerDescription}</span>
+                        </ul>
+                      </div>
+                    </div>
                   ))}
-                </ul>
+                  <SessionDialog
+                    open={isSessionDialogOpen}
+                    onClose={() => setIsSessionDialogOpen(false)}
+                    session={selectedSession}
+                  />
+                </div>
               </TabPanel>
             ))}
           </TabPanels>
