3553513caf260f324f818ec4c6296cf8e47d2e47
Grayash <k_2684@naver.com>
2024-08-25 14:45:29 +0900

    Update SessionSection background color transition

--- src/app/components/SessionSection/index.tsx
+++ src/app/components/SessionSection/index.tsx
@@ -24,8 +24,8 @@ export default function SessionSection() {
     <>
       <div
         className={cn(
-          "h-screen-dvh bg-gray-4000/50 fixed left-0 top-0 -z-10 flex w-full items-center justify-center transition-all duration-500",
-          inView ? "bg-gray-400/50" : "bg-gray-400/0"
+          "bg-gray-4000/50 fixed left-0 top-0 -z-10 flex w-full items-center justify-center transition-all duration-500 h-screen-dvh",
+          inView ? "bg-slate-500/60" : "bg-gray-400/0"
         )}
       />
       <motion.section
