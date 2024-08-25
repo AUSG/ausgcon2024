cee708b36d54d9eb46ce51b07f0e7491749d9be9
Grayash <k_2684@naver.com>
2024-08-25 14:44:43 +0900

    Update Background component to use a new Spline 3D scene

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -2,8 +2,8 @@ import Spline from "@splinetool/react-spline/next";
 
 export default function Background() {
   return (
-    <div className="fixed left-0 top-0 -z-20 flex h-full w-full items-center justify-center">
-      <Spline scene="https://prod.spline.design/y3eKWYgU8LAe4zuH/scene.splinecode" />
+    <div className="fixed left-0 top-0 -z-20 flex h-full w-full items-center justify-center bg-scene">
+      <Spline scene="https://prod.spline.design/jvfn5O02IqRZEfqP/scene.splinecode" />
     </div>
   );
 }
