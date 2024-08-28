21707d9d1d7cf2dd1b75887c880f93d9d06e0fbe
Grayash <k_2684@naver.com>
2024-08-29 00:27:29 +0900

    Add Footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="bg-neutral text-neutral-content flex w-full items-center p-4">
+      <div className="flex w-full items-center justify-between">
+        <div className="flex items-center">
+          <Image src="/short_logo.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className="flex items-center">
+          <p>@ AUSG {new Date().getFullYear()} All right reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Backdrop from "./components/Backdrop";
 import Background from "./components/Background";
 import FAQSection from "./components/FAQSection";
+import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import SessionSection from "./components/SessionSection";
 
@@ -12,6 +13,7 @@ export default function Home() {
         <SessionSection />
         <FAQSection />
         <Background />
+        <Footer />
       </Backdrop>
     </main>
   );
