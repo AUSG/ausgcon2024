0eec1ddddcaa4ca7c8e439d6edf488f97f2537af
Grayash <k_2684@naver.com>
2024-08-29 00:02:49 +0900

    Update Background component to use cloud.spline asset

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline/next";
 export default function Background() {
   return (
     <div className="fixed left-0 top-0 -z-20 flex h-full w-full items-center justify-center bg-scene">
-      <Spline scene="https://prod.spline.design/jvfn5O02IqRZEfqP/scene.splinecode" />
+      <Spline scene="/cloud.spline" />
     </div>
   );
 }
