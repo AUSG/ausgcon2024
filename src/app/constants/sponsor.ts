025d0684f9b5edf8b9888f97e71f20a594a827fc
Grayash <k_2684@naver.com>
2024-09-01 01:43:37 +0900

    Add sponsor list constant and assets

---
+++ src/app/constants/sponsor.ts
@@ -0,0 +1,22 @@
+export const sponsorList = [
+  {
+    name: "AWSKRUG",
+    image: "/sponsor/awskrug.png",
+    url: "https://awskrug.github.io",
+  },
+  {
+    name: "Notion",
+    image: "/sponsor/notion.png",
+    url: "https://www.notion.so/ko/product",
+  },
+  {
+    name: "Umoh",
+    image: "/sponsor/umoh.png",
+    url: "https://umoh.io",
+  },
+  {
+    name: "이지스퍼블리싱",
+    image: "/sponsor/easyspub.png",
+    url: "https://www.easyspub.co.kr",
+  },
+];
