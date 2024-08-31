982e8220f5ec9f47b712165934e8fea1294f8055
Grayash <k_2684@naver.com>
2024-08-31 23:34:47 +0900

    Add FAQSection component and faq constant

--- src/app/components/FAQSection/FAQList.tsx
+++ src/app/components/FAQSection/FAQList.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { faqList } from "@/app/constants/faq";
 import {
   Disclosure,
   DisclosureButton,
@@ -11,29 +12,25 @@ import FadeIn from "../FadeIn";
 export default function FAQList() {
   return (
     <FadeIn className="flex w-full justify-center px-4 pt-10">
-      <div className="w-full max-w-md divide-y divide-white/5 rounded-xl bg-white/20">
-        <Disclosure as="div" className="p-6" defaultOpen={true}>
-          <DisclosureButton className="group flex w-full items-center justify-between">
-            <span className="text-sm/6 font-medium text-white group-data-[hover]:text-white/80">
-              What is your refund policy?
-            </span>
-            <ChevronDownIcon className="size-5 fill-white/60 group-data-[open]:rotate-180 group-data-[hover]:fill-white/50" />
-          </DisclosureButton>
-          <DisclosurePanel className="mt-2 text-sm/5 text-white/50">
-            If you're unhappy with your purchase, we'll refund you in full.
-          </DisclosurePanel>
-        </Disclosure>
-        <Disclosure as="div" className="p-6">
-          <DisclosureButton className="group flex w-full items-center justify-between">
-            <span className="text-sm/6 font-medium text-white group-data-[hover]:text-white/80">
-              Do you offer technical support?
-            </span>
-            <ChevronDownIcon className="size-5 fill-white/60 group-data-[open]:rotate-180 group-data-[hover]:fill-white/50" />
-          </DisclosureButton>
-          <DisclosurePanel className="mt-2 text-sm/5 text-white/50">
-            No.
-          </DisclosurePanel>
-        </Disclosure>
+      <div className="w-full divide-y divide-white/5 rounded-xl bg-white/20">
+        {faqList.map((faq) => (
+          <Disclosure
+            key={faq.question}
+            defaultOpen={true}
+            as="div"
+            className="p-6"
+          >
+            <DisclosureButton className="group flex w-full items-center justify-between">
+              <span className="text-sm/6 font-medium text-white group-data-[hover]:text-white/80">
+                {faq.question}
+              </span>
+              <ChevronDownIcon className="size-5 fill-white/60 group-data-[open]:rotate-180 group-data-[hover]:fill-white/50" />
+            </DisclosureButton>
+            <DisclosurePanel className="mt-2 whitespace-pre-wrap text-sm/5 text-white/70">
+              {faq.answer}
+            </DisclosurePanel>
+          </Disclosure>
+        ))}
       </div>
     </FadeIn>
   );
