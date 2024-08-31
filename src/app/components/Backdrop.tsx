b95aeca9f3d16ec26a3b50aa42a9f2d7f6d3cf8f
Grayash <k_2684@naver.com>
2024-09-01 00:24:45 +0900

    Update Backdrop component styling and transition duration

--- src/app/components/Backdrop.tsx
+++ src/app/components/Backdrop.tsx
@@ -12,8 +12,8 @@ export default function Backdrop({ children }: { children: React.ReactNode }) {
     <>
       <div
         className={cn(
-          "bg-gray-4000/50 fixed left-0 top-0 -z-10 flex h-full w-full items-center justify-center transition-all duration-500",
-          inView ? "bg-slate-500/60" : "bg-gray-400/0"
+          "bg-gray-4000/50 fixed left-0 top-0 -z-10 flex h-full w-full items-center justify-center transition-colors duration-500",
+          inView && "bg-gray-700/80"
         )}
       />
       <div ref={ref}>{children}</div>
