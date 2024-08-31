f605e385f778ea12ca84e0ce649a89ad66c62bf8
Grayash <k_2684@naver.com>
2024-08-31 23:31:10 +0900

    Update Backdrop component styling and structure

--- src/app/components/Backdrop.tsx
+++ src/app/components/Backdrop.tsx
@@ -12,13 +12,11 @@ export default function Backdrop({ children }: { children: React.ReactNode }) {
     <>
       <div
         className={cn(
-          "bg-gray-4000/50 fixed left-0 top-0 -z-10 flex w-full items-center justify-center transition-all duration-500 h-screen-dvh",
+          "bg-gray-4000/50 fixed left-0 top-0 -z-10 flex h-full w-full items-center justify-center transition-all duration-500",
           inView ? "bg-slate-500/60" : "bg-gray-400/0"
         )}
       />
-      <div ref={ref} className="min-h-full">
-        {children}
-      </div>
+      <div ref={ref}>{children}</div>
     </>
   );
 }
