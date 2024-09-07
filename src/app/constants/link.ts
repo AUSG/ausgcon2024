d1ccb775b350af4ead6a0527bfe42c44611f63e2
Grayash <k_2684@naver.com>
2024-09-07 17:42:53 +0900

    Add Facebook link to Footer component

--- src/app/constants/link.ts
+++ src/app/constants/link.ts
@@ -2,5 +2,6 @@ export const HOMEPAGE_LINK = "https://ausg.me";
 export const YOUTUBE_LINK = "https://www.youtube.com/@ausg";
 export const LINKED_IN_LINK = "https://www.linkedin.com/company/ausg-kr";
 export const INSTAGRAM_LINK = "https://www.instagram.com/ausg.awskrug";
+export const FACEBOOK_LINK = "https://www.facebook.com/ausgkr";
 export const GITHUB_LINK = "https://github.com/ausg";
 export const EMAIL = "ausg.awskrug@gmail.com";
